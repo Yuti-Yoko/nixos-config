a9115332736d6691017732531c777d944d4f02fa
Yuti-Yoko <shfayz0777@gmail.com>
2025-09-03 17:51:49 +0500

    chore: experimental config for laptop

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    # NixOS official package source, using the nixos-25.05 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = input @ {
+    nixpkgs,
+    home-manager,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      name = "nix-dev-shell";
+      buildInputs = [pkgs.alejandra];
+      shellHook = ''
+        echo "Welcome to the Nix dev shell with Alejandra formatter!"
+      '';
+    };
+
+    nixosConfigurations = {
+      ilumix = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.ilumix = ./home.nix;
+              backupFileExtension = "backup";
+            };
+          }
+        ];
+      };
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,48 @@
+{pkgs, ...}: {
+  imports = [
+    ./modules/home/zed.nix
+    #./home/modules/alacritty.nix
+  ];
+  # TODO please change the username & home directory to your own
+  home.username = "ilumix";
+  home.homeDirectory = "/home/ilumix";
+
+  # link the configuration file in current directory to the specified location in home directory
+  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
+
+  # link all files in `./scripts` to `~/.config/i3/scripts`
+  # home.file.".config/i3/scripts" = {
+  #   source = ./scripts;
+  #   recursive = true;   # link recursively
+  #   executable = true;  # make all files executable
+  # };
+
+  # encode the file content in nix configuration file directly
+  # home.file.".xxx".text = ''
+  #     xxx
+  # '';
+
+  # set cursor size and dpi for 4k monitor
+  xresources.properties = {
+    "Xcursor.size" = 16;
+    "Xft.dpi" = 172;
+  };
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    # here is some command line tools I use frequently
+    # feel free to add your own or remove some of them
+
+    neofetch
+    telegram-desktop
+    thunderbird
+    inetutils
+    obsidian
+    spotify
+    discord
+    vscode
+    osu-lazer
+    alejandra
+  ];
+  home.stateVersion = "25.05";
+}
